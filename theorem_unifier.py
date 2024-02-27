eab3edf515dee2b22332f8378b9716eeb4a61542
Jostein Bakkevig <jostbak@stud.ntnu.no>
2024-02-27 14:50:08 +0100

    Refactored theorem unifier to no longer need to keep track of negation

--- theorem_unifier.py
+++ theorem_unifier.py
@@ -15,7 +15,7 @@ we then iterate over the list of theorems. If a theorem is invalid, we discard i
 
 def list_all():
     """Generate a list of all possible theorems"""
-    theorems = itertools.product(itertools.permutations(["M","G","N","V"]), itertools.product(["A","E"],repeat=4), itertools.product(range(2),repeat=5))
+    theorems = itertools.product(itertools.permutations(["M","G","N","V"]), itertools.product(["A","E"],repeat=5))
     return theorems
 
 def merge_equivalence(classes, i, j):
@@ -24,58 +24,63 @@ def merge_equivalence(classes, i, j):
     del classes[j]
 
 def equivalent(t1, t2):
-    # Placeholder:
-    eq = True
+    # first check if ordering is same and quantifiers are inverted
+    if t1[0]==t2[0]:
+        # both have same variable order
+        for i in range(5):
+            if t1[1][i] == t2[1][i]:
+                # quantifiers are not inverted, break
+                break
+            else:
+                if i == 4:
+                    return True
     i = 0
-    inv = False #flags whether previous check found an inversion, in which case we allow the leading negator to be different
     while i <= 4:
         if i == 4:
-            # the final parity must be equal unless the last check found an inversion
-            if inv or (t1[2][i] == t2[2][i]):
+            # the final quantifier must be equal
+            if (t1[1][i] == t2[1][i]):
                 break
             else:
                 return False
-        if inv or (i==0) or (t1[2][i] == t2[2][i]):
-            # we have either:
-            #    same parity
-            #    first type in statement, so we don't care for parity
-            #    prev type was equiv under inversion, so parity was already checked (and must be different)
-            inv = False # clear flag
-            if t1[1][i] == t2[1][i]:
-                # same quantifier
-                if t1[0][i] == t2[0][i]:
-                    # same type
-                    # equiv under equality
-                    # check next level
-                    i+= 1
-                    continue
-                else:
-                    # different type
-                    # must be a swap
-                    if  (t1[0][i] == t2[0][i+1]) and \
-                        (t1[0][i+1] == t2[0][i]) and \
-                        (t1[1][i] == t1[1][i+1] == t2[1][i+1]) and \
-                        (t1[2][i+1] == t2[2][i+1] == 0):
-                        # we have a swap
-                        # this means we also checked the next type, quantifier and parity,
-                        # so we can skip the next type
-                        i += 2
-                        continue
-        else:
-            # different parity, we must have inversion, or they are non-equivalent
-            if (t1[0][i] == t2[0][i]) and \
-               (t1[1][i] != t2[1][i]) and \
-               (t1[2][i+1] != t2[2][i+1]):
-                # same type, different quantifier, different parity before and after
-                # we have equiv under inversion
-                inv = True
-                i += 1
+        if t1[1][i] == t2[1][i]:
+            # same quantifier
+            if t1[0][i] == t2[0][i]:
+                # same type
+                # equiv under equality
+                # check next level
+                i+= 1
                 continue
+            else:
+                # different type
+                # must be a swap
+                if  (t1[0][i] == t2[0][i+1]) and \
+                    (t1[0][i+1] == t2[0][i]) and \
+                    (t1[1][i] == t1[1][i+1] == t2[1][i+1]):
+                    # we have a swap
+                    # this means we also checked the next type, quantifier and parity,
+                    # so we can skip the next type
+                    i += 2
+                    continue
         # all cases failed, so no equivalence
         return False
     return True
 
-def filter_theorem(theorem):
+def pre_filter(theorem):
+    return False
+
+forbidden_strings = ["EGAM"]
+
+def post_filter(theorem):
+    # interleave quantifiers and types into a single string
+    # ls =  theorem[0] + theorem[1]
+    # ls[::2] = theorem[1]
+    # ls[1::2] = theorem[0]
+    ls = [theorem[1][i//2] if i%2 == 0 else theorem[0][i//2] for i in range(len(theorem[0])+len(theorem[1]))]
+    ls.append('A')
+    string = ''.join(ls)
+    for forbidden in forbidden_strings:
+        if forbidden in string:
+            return True
     return False
 
 
@@ -89,7 +94,7 @@ if __name__ == "__main__":
     classes = dict()
     for theorem in itr:
         # pre-filter
-        if(filter_theorem(theorem)):
+        if(pre_filter(theorem)):
             skipped += 1
             continue
         equiv = None
@@ -119,6 +124,18 @@ if __name__ == "__main__":
             classes[class_index] = [theorem]
             class_index += 1
         count += 1
-        if count % 1000 == 0:
+        if count % 100 == 0:
             print(f"{count} theorems searched, {skipped} filtered. {len(classes)} equivalence classes, {merged} merged")
-    print(len(classes.keys()))
+    print(f"Classification done: {count} theorems searched, {skipped} filtered. {len(classes)} equivalence classes found, {merged} merged")
+    to_filter = []
+    post_filtered = 0
+    for eqclass, eqtheorems in classes.items():
+        for theorem in eqtheorems:
+            if post_filter(theorem):
+                to_filter.append(eqclass)
+                break
+    for eqclass in to_filter:
+        post_filtered += len(classes[eqclass])
+        del classes[eqclass]
+    print(f"Filtering done: {len(to_filter)} classes filtered, removing {post_filtered} theorems")
+    print(f"Remaining classes: {len(classes)}, containing {count-post_filtered} theorems")
